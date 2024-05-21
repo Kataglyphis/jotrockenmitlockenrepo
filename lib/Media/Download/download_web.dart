d167a8a2a647b5b599a2365b73d169d1d024c5dd
Jonas Heinle <jonasheinle@googlemail.com>
2024-05-21 19:27:34 +0200

    fixed naming bug in download button

--- lib/Media/Download/download_web.dart
+++ lib/Media/Download/download_web.dart
@@ -1,12 +1,22 @@
 import 'package:web/web.dart' as web;
 import 'package:flutter/foundation.dart';
 
+String trimAfterLastSlash(String input) {
+  int lastSlashIndex = input.lastIndexOf('/');
+  if (lastSlashIndex == -1) {
+    // If no slash is found, return the original string
+    return input;
+  }
+  // Return the substring after the last slash
+  return input.substring(lastSlashIndex + 1);
+}
+
 void myPluginDownload(String url) {
   if (kReleaseMode) {
     url = "assets/$url";
   }
   web.HTMLAnchorElement anchorElement = web.HTMLAnchorElement();
+  anchorElement.download = trimAfterLastSlash(url);
   anchorElement.href = url;
-  anchorElement.download = url;
   anchorElement.click();
 }
