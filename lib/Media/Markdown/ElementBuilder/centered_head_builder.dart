30e15c7b4990e270341db8d18cb42325f41a5cb6
Jonas Heinle <jonasheinle@googlemail.com>
2024-07-06 15:49:56 +0200

    updated dependencies and removed overlfowing markdown error

--- lib/Media/Markdown/ElementBuilder/centered_head_builder.dart
+++ lib/Media/Markdown/ElementBuilder/centered_head_builder.dart
@@ -5,14 +5,19 @@ import 'package:markdown/markdown.dart' as md;
 class CenteredHeaderBuilder extends MarkdownElementBuilder {
   @override
   Widget visitText(md.Text text, TextStyle? preferredStyle) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(text.text, style: preferredStyle),
-        const SizedBox(
-          height: 80,
-        )
-      ],
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Flexible(child: Text(text.text, style: preferredStyle)),
+            const SizedBox(
+              height: 80,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
