e3615b04aed82fb9c11e8b784776ee9a3092ed87
Jonas Heinle <jonasheinle@googlemail.com>
2025-05-21 08:56:57 +0200

    temorarly removing math fork for not being mainteined

--- lib/Media/Markdown/ElementBuilder/code_element_builder.dart
+++ lib/Media/Markdown/ElementBuilder/code_element_builder.dart
@@ -4,18 +4,19 @@ import 'package:flutter_highlighter/flutter_highlighter.dart';
 import 'package:flutter_highlighter/themes/github.dart';
 import 'package:flutter_highlighter/themes/dracula.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
-import 'package:flutter_math_fork/flutter_math.dart';
+// import 'package:flutter_math_fork/flutter_math.dart';
 import 'package:jotrockenmitlockenrepo/Decoration/centered_box_decoration.dart';
 import 'package:markdown/markdown.dart' as md;
 import 'package:jotrockenmitlockenrepo/Media/copy_button.dart';
 import 'package:jotrockenmitlockenrepo/constants.dart';
 
 class CodeElementBuilder extends MarkdownElementBuilder {
-  CodeElementBuilder(
-      {required this.colorSelectedBg,
-      required this.useLightMode,
-      required this.colorSelectedPrimary,
-      required this.markdownPageWidth});
+  CodeElementBuilder({
+    required this.colorSelectedBg,
+    required this.useLightMode,
+    required this.colorSelectedPrimary,
+    required this.markdownPageWidth,
+  });
 
   Map<String, TextStyle> getCodeTheme() {
     if (useLightMode) {
@@ -41,8 +42,12 @@ class CodeElementBuilder extends MarkdownElementBuilder {
   double markdownPageWidth;
 
   @override
-  Widget visitElementAfterWithContext(BuildContext context, md.Element element,
-      TextStyle? preferredStyle, TextStyle? parentStyle) {
+  Widget visitElementAfterWithContext(
+    BuildContext context,
+    md.Element element,
+    TextStyle? preferredStyle,
+    TextStyle? parentStyle,
+  ) {
     var language = '';
 
     if (element.attributes['class'] != null) {
@@ -52,69 +57,72 @@ class CodeElementBuilder extends MarkdownElementBuilder {
 
     if (language == 'math') {
       return Center(
-          child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Column(
-          children: [
-            CenteredBoxDecoration(
-              insets: const EdgeInsets.all(2),
-              borderWidth: 1,
-              color: colorSelectedPrimary,
-              child: Container(
-                color: colorSelectedBg,
-                child: Math.tex(
-                  element.textContent,
-                  textStyle: preferredStyle,
-                  textScaleFactor: 1.6,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Column(
+            children: [
+              CenteredBoxDecoration(
+                insets: const EdgeInsets.all(2),
+                borderWidth: 1,
+                color: colorSelectedPrimary,
+                child: Container(
+                  color: colorSelectedBg,
+                  child: Text(element.textContent),
+                  // Math.tex(
+                  //   element.textContent,
+                  //   textStyle: preferredStyle,
+                  //   textScaleFactor: 1.6,
+                  // ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ));
+      );
     }
     return Center(
       child: Column(
         children: [
           Container(
-              constraints: const BoxConstraints(
-                maxWidth: 700,
+            constraints: const BoxConstraints(maxWidth: 700),
+            child: Card(
+              margin: EdgeInsets.zero,
+              elevation: 0,
+              color: (markdownPageWidth > narrowScreenWidthThreshold)
+                  ? Theme.of(
+                      context,
+                    ).colorScheme.surfaceContainerHighest.withOpacity(0.3)
+                  : null,
+              child: CenteredBoxDecoration(
+                color: colorSelectedPrimary,
+                child: Stack(
+                  children: [
+                    Center(
+                      child: HighlightView(
+                        tabSize: 4,
+                        // The original code to be highlighted
+                        element.textContent,
+                        // Specify language
+                        // It is recommended to give it a value for performance
+                        language: language,
+                        theme: getCodeTheme(),
+                        textStyle: preferredStyle,
+                        // Specify padding
+                        padding: const EdgeInsets.all(8),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CopyButton(text: element.textContent),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              child: Card(
-                  margin: EdgeInsets.zero,
-                  elevation: 0,
-                  color: (markdownPageWidth > narrowScreenWidthThreshold)
-                      ? Theme.of(context)
-                          .colorScheme
-                          .surfaceContainerHighest
-                          .withOpacity(0.3)
-                      : null,
-                  child: CenteredBoxDecoration(
-                      color: colorSelectedPrimary,
-                      child: Stack(children: [
-                        Center(
-                          child: HighlightView(
-                            tabSize: 4,
-                            // The original code to be highlighted
-                            element.textContent,
-                            // Specify language
-                            // It is recommended to give it a value for performance
-                            language: language,
-                            theme: getCodeTheme(),
-                            textStyle: preferredStyle,
-                            // Specify padding
-                            padding: const EdgeInsets.all(8),
-                          ),
-                        ),
-                        Align(
-                            alignment: Alignment.topRight,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: CopyButton(
-                                text: element.textContent,
-                              ),
-                            ))
-                      ]))))
+            ),
+          ),
         ],
       ),
     );
--- lib/Media/Markdown/ElementBuilder/latex_element_builder.dart
+++ lib/Media/Markdown/ElementBuilder/latex_element_builder.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
-import 'package:flutter_math_fork/flutter_math.dart';
+// import 'package:flutter_math_fork/flutter_math.dart';
 import 'package:markdown/markdown.dart' as md;
 
 class LatexElementBuilder extends MarkdownElementBuilder {
-  LatexElementBuilder({
-    this.textStyle,
-    this.textScaleFactor,
-  });
+  LatexElementBuilder({this.textStyle, this.textScaleFactor});
 
   /// The style to apply to the text.
   final TextStyle? textStyle;
@@ -27,24 +24,26 @@ class LatexElementBuilder extends MarkdownElementBuilder {
       return const SizedBox();
     }
 
-    MathStyle mathStyle;
-    switch (element.attributes['MathStyle']) {
-      case 'text':
-        mathStyle = MathStyle.text;
-      case 'display':
-        mathStyle = MathStyle.display;
-      default:
-        mathStyle = MathStyle.text;
-    }
+    // MathStyle mathStyle;
+    // switch (element.attributes['MathStyle']) {
+    //   case 'text':
+    //     mathStyle = MathStyle.text;
+    //   case 'display':
+    //     mathStyle = MathStyle.display;
+    //   default:
+    //     mathStyle = MathStyle.text;
+    // }
 
-    return Text.rich(WidgetSpan(
-      alignment: PlaceholderAlignment.middle,
-      child: SelectableMath.tex(
-        text,
-        textStyle: textStyle,
-        mathStyle: mathStyle,
-        textScaleFactor: textScaleFactor,
-      ),
-    ));
+    return Text(text);
+
+    // return Text.rich(WidgetSpan(
+    //   alignment: PlaceholderAlignment.middle,
+    //   child: SelectableMath.tex(
+    //     text,
+    //     textStyle: textStyle,
+    //     mathStyle: mathStyle,
+    //     textScaleFactor: textScaleFactor,
+    //   ),
+    // ));
   }
 }
